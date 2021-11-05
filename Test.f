5c7c913771b0ed36446d1ddfcb6dd3ac53ddef73
Evan Hanzelman <ehanz1997@gmail.com>
2021-11-05 10:43:21 -0400

    Triangleeeeeee

---
+++ Test.f
@@ -0,0 +1,31 @@
+      module class_Triangle
+        implicit none
+        private
+        public :: Triangle, triangle_area, triangle_print
+
+       type Triangle
+         real :: bh
+        end type Triangle
+       contains
+        function triangle_area(this) result(area)
+         type(Triangle), intent(in) :: this
+         real :: area
+         area = 0.5 * this%bh
+        end function triangle_area
+
+       subroutine triangle_print(this)
+        type(Triangle), intent(in) :: this
+        real :: area
+        area = triangle_area(this)
+        print * , 'Triangle bh=  ', this%bh, ' Area = ', area
+       end subroutine triangle_print
+      end module class_Triangle
+
+      program triangle_test
+       use class_Triangle
+       implicit none
+
+       type (Triangle) :: t
+       t = Triangle(6)
+       call triangle_print (t)
+      end program triangle_test
